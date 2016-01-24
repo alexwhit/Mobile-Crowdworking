469a842283d58087cc69ff19e14f36f2f5efb4c1
Kate Miller <kate.miller237@gmail.com>
2016-01-24 15:40:07 -0500

    imports selenium for webdriver

--- M-Crowdsourcing/app/src/main/java/cis400/m_crowdsourcing/SearchActivity.java
+++ M-Crowdsourcing/app/src/main/java/cis400/m_crowdsourcing/SearchActivity.java
@@ -1,9 +1,11 @@
 package cis400.m_crowdsourcing;
+//package org.openqa.selenium.*;
 
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,23 +13,29 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.os.AsyncTask;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-
+//import selendroid;
+/*
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+*/
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.net.URL;
 
 
 public class SearchActivity extends ListActivity {
@@ -65,6 +73,32 @@ public class SearchActivity extends ListActivity {
 
 
     }
+
+   /* public class Example  {
+        public static void main(String[] args) {
+            // Create a new instance of the html unit driver
+            // Notice that the remainder of the code relies on the interface,
+            // not the implementation.
+            WebDriver driver = new HtmlUnitDriver();
+
+            // And now use this to visit Google
+            driver.get("http://www.google.com");
+
+            // Find the text input element by its name
+            WebElement element = driver.findElement(By.name("q"));
+
+            // Enter something to search for
+            element.sendKeys("Cheese!");
+
+            // Now submit the form. WebDriver will find the form for us from the element
+            element.submit();
+
+            // Check the title of the page
+            System.out.println("Page title is: " + driver.getTitle());
+
+            driver.quit();
+        }
+    } */
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
@@ -122,16 +156,30 @@ public class SearchActivity extends ListActivity {
         protected Long doInBackground(URL... urls) {
             String preview_url = urls[0].toString();
             try {
-                Document doc = Jsoup.connect(preview_url).get();
                 String signature = "";
+                Connection connect = Jsoup.connect(preview_url);
 
-                Elements signatures = doc.select("input[name = signature]");
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                Document doc = connect.get();
+
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                Elements signatures = doc.select("input");
                 System.out.println(signatures.size());
                 System.out.println("signatures size just printed");
                 for (Element element : signatures) {
                     System.out.println(element);
-                    System.out.println("signature: " + element.attr("value"));
-                    signature = element.attr("value");
+                   // System.out.println("signature: " + element.attr("value"));
+                    //signature = element.attr("value");
                 }
 
                 Elements links = doc.select("a[id = hitExternalNextAcceptLink]");
